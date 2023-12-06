48ac70e209c5e225fe1b48c2e2dff2b363baecb2
Fellipe Marcel <fmrclms@gmail.com>
2023-12-06 09:59:57 -0300

    fix: adds javadoc to TaskServiceInterface

--- src/main/java/com/betrybe/taskmanager/service/TaskServiceInterface.java
+++ src/main/java/com/betrybe/taskmanager/service/TaskServiceInterface.java
@@ -4,6 +4,9 @@ import com.betrybe.taskmanager.dto.TaskDto;
 import com.betrybe.taskmanager.model.TaskModel;
 import java.util.List;
 
+/**
+ * The interface Task service interface.
+ */
 public interface TaskServiceInterface {
 
   TaskDto modelToDto(TaskModel task);
