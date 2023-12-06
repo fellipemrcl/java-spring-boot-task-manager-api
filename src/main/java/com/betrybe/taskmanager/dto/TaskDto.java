5828945647ae84e9c9fae199389fcf70a1c8f0a2
Fellipe Marcel <fmrclms@gmail.com>
2023-12-06 09:19:10 -0300

    feat: created TaskDto

---
+++ src/main/java/com/betrybe/taskmanager/dto/TaskDto.java
@@ -0,0 +1,14 @@
+package com.betrybe.taskmanager.dto;
+
+/**
+ * The type Task dto.
+ */
+public record TaskDto(
+    String id,
+    String title,
+    String description,
+    String ownerName,
+    boolean isComplete
+) {
+
+}
